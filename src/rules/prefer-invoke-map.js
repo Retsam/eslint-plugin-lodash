804785dd8d51ae8f50eab647ef538ba994131925
omerganim <ganimomer@gmail.com>
2016-07-22 16:30:39 +0300

    update rules to new rule format (requires eslint >= 2.10)

--- src/rules/prefer-invoke-map.js
+++ src/rules/prefer-invoke-map.js
@@ -3,23 +3,32 @@
  */
 'use strict'
 
+/**
+ * @fileoverview Rule to check if a call to map should be a call to invokeMap
+ */
 //------------------------------------------------------------------------------
 // Rule Definition
 //------------------------------------------------------------------------------
 
-module.exports = function (context) {
-    const {getLodashMethodVisitor, isCallToMethod} = require('../util/lodashUtil')
-    const {isCallFromObject, getValueReturnedInFirstLine, getFirstParamName} = require('../util/astUtil')
-    const settings = require('../util/settingsUtil').getSettings(context)
-    function isFunctionMethodCallOfParam(func) {
-        return isCallFromObject(getValueReturnedInFirstLine(func), getFirstParamName(func))
-    }
+module.exports = {
+    meta: {
+        docs: {}
+    },
 
-    return {
-        CallExpression: getLodashMethodVisitor(settings, (node, iteratee) => {
-            if (isCallToMethod(node, settings.version, 'map') && isFunctionMethodCallOfParam(iteratee)) {
-                context.report(node, 'Prefer _.invokeMap over map to a method call.')
-            }
-        })
+    create(context) {
+        const {getLodashMethodVisitor, isCallToMethod} = require('../util/lodashUtil')
+        const {isCallFromObject, getValueReturnedInFirstLine, getFirstParamName} = require('../util/astUtil')
+        const settings = require('../util/settingsUtil').getSettings(context)
+        function isFunctionMethodCallOfParam(func) {
+            return isCallFromObject(getValueReturnedInFirstLine(func), getFirstParamName(func))
+        }
+
+        return {
+            CallExpression: getLodashMethodVisitor(settings, (node, iteratee) => {
+                if (isCallToMethod(node, settings.version, 'map') && isFunctionMethodCallOfParam(iteratee)) {
+                    context.report(node, 'Prefer _.invokeMap over map to a method call.')
+                }
+            })
+        }
     }
 }
--- src/rules/unwrap.js
+++ src/rules/unwrap.js
@@ -3,23 +3,28 @@
  */
 'use strict'
 
+/**
+ * @fileoverview Rule to ensure a lodash chain ends
+ */
 // ------------------------------------------------------------------------------
 // Rule Definition
 // ------------------------------------------------------------------------------
 
-module.exports = function (context) {
-    const {isExplicitMethodChaining, isChainBreaker, isCallToMethod, isChainable, isEndOfChain} = require('../util/lodashUtil')
-    const settings = require('../util/settingsUtil').getSettings(context)
-    function isExplicitChainWithoutBreaker(node) {
-        return isExplicitMethodChaining(node, settings.version) && !isChainBreaker(node, settings.version)
-    }
-    function isEvaluatedWhenLazy(node) {
-        return isCallToMethod(node, settings.version, 'commit') || !isChainable(node, settings.version)
-    }
-    return {
-        CallExpression(node) {
-            if (isEndOfChain(node, settings.pragma, settings.version) && (!isEvaluatedWhenLazy(node) || isExplicitChainWithoutBreaker(node))) {
-                context.report(node, 'Missing unwrapping at end of chain')
+module.exports = {
+    create(context) {
+        const {isExplicitMethodChaining, isChainBreaker, isCallToMethod, isChainable, isEndOfChain} = require('../util/lodashUtil')
+        const settings = require('../util/settingsUtil').getSettings(context)
+        function isExplicitChainWithoutBreaker(node) {
+            return isExplicitMethodChaining(node, settings.version) && !isChainBreaker(node, settings.version)
+        }
+        function isEvaluatedWhenLazy(node) {
+            return isCallToMethod(node, settings.version, 'commit') || !isChainable(node, settings.version)
+        }
+        return {
+            CallExpression(node) {
+                if (isEndOfChain(node, settings.pragma, settings.version) && (!isEvaluatedWhenLazy(node) || isExplicitChainWithoutBreaker(node))) {
+                    context.report(node, 'Missing unwrapping at end of chain')
+                }
             }
         }
     }
